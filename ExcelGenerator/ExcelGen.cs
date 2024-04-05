7f2c8cc179033317427811bbee18e80b47d7ecd0
massimiliano aronica <massimiliano.aronica@hotmail.com>
2024-04-05 09:35:40 +0200

    private excelgen

--- ExcelGenerator/ExcelGen.cs
+++ ExcelGenerator/ExcelGen.cs
@@ -33,143 +33,144 @@ namespace ExcelGenerator
         {
             return _gen.Create<T>(items);
         }
-    }
-    public class ExcelGen 
-    {
-        public String SheetName { get; set; }
-        public string SheetTitle { get; set; }
-        public string SheetSubTitle { get; set; }
-
-        private string? HasColumnNameAttribute(PropertyInfo prop)
+        private class ExcelGen
         {
-            var attributes = prop.GetCustomAttributes(true);
-            if (attributes.Any())
+            public string SheetName { get; set; }
+            public string SheetTitle { get; set; }
+            public string SheetSubTitle { get; set; }
+
+            private string? HasColumnNameAttribute(PropertyInfo prop)
             {
-                foreach (var att in attributes)
+                var attributes = prop.GetCustomAttributes(true);
+                if (attributes.Any())
                 {
-                    ColumnNameAttribute? nameAttribute = att as ColumnNameAttribute;
-                    if (nameAttribute != null)
+                    foreach (var att in attributes)
                     {
-                        return nameAttribute.Name;
+                        ColumnNameAttribute? nameAttribute = att as ColumnNameAttribute;
+                        if (nameAttribute != null)
+                        {
+                            return nameAttribute.Name;
+                        }
                     }
                 }
+                return null;
             }
-            return null;
-        }
-        private string? HasDateFormatAttribute(PropertyInfo prop)
-        {
-            var attributes = prop.GetCustomAttributes(true);
-            if (attributes.Any())
+            private string? HasDateFormatAttribute(PropertyInfo prop)
             {
-                foreach (var att in attributes)
+                var attributes = prop.GetCustomAttributes(true);
+                if (attributes.Any())
                 {
-                    DateFormatAttribute? dateFormat = att as DateFormatAttribute;
-                    if (dateFormat != null)
+                    foreach (var att in attributes)
                     {
-                        return dateFormat.Format;
-                    }
-                }
-            }
-            return null;
-        }
-        private string[]? HasTrueFalseAttribute(PropertyInfo prop)
-        {
-            var attributes = prop.GetCustomAttributes(true);
-            if (attributes.Any())
-            {
-                foreach (var att in attributes)
-                {
-                    TrueFalseAttribute? truefalse = att as TrueFalseAttribute;
-                    if (truefalse != null)
-                    {
-                        return truefalse.TF.Split('_');
-
-                    }
-                }
-            }
-            return null;
-        }
-
-        private PropertyInfo[] GetPropertiesOrderedByAttribute(PropertyInfo[] props)
-        {
-            bool AreOrdered = props.Any(x => x.CustomAttributes.Any(z => z.AttributeType == typeof(ColumnOrderAttribute)));
-            if (AreOrdered)
-            {
-                props = props
-                      .Select(x => new { prop = x, ord = x.GetCustomAttribute<ColumnOrderAttribute>()?.Order })
-                      .OrderBy(z => z.ord == null).ThenBy(z => z.ord).Select(a => a.prop).ToArray();
-            }
-            return props;
-        }
-        public byte[] Create<T>(List<T> items)
-        {
-            var wbook = new XLWorkbook();
-
-            var ws = wbook.Worksheets.Add(SheetName);
-            int row = 1;
-            if (! String.IsNullOrWhiteSpace(this.SheetTitle))
-            {
-                ws.Cell(row, 1).SetValue(this.SheetTitle);
-                ws.Cell(row, 1).Style.Font.FontSize = 20;
-                row++;
-            }
-            if (!String.IsNullOrWhiteSpace(this.SheetSubTitle))
-            {
-                ws.Cell(row, 1).SetValue(this.SheetSubTitle);
-                ws.Cell(row, 1).Style.Font.FontSize = 16;
-                row++;
-            }
-            var props = typeof(T).GetProperties();
-
-            props = GetPropertiesOrderedByAttribute(props);
-
-            
-            int col = 1;
-            foreach (var prop in props)
-            {
-                ws.Cell(row, col).SetValue(HasColumnNameAttribute(prop) ?? prop.Name);
-                ws.Cell(row, col).Style.Font.Bold = true;
-                ws.Cell(row, col).Style.Fill.SetBackgroundColor(XLColor.LightGray);
-                col++;
-            }
-
-
-            foreach (var item in items)
-            {
-                row++;
-                col = 0;
-                foreach (var prop in props)
-                {
-                    col++;
-                    object? value = item?.GetType().GetProperty(prop.Name)?.GetValue(item);
-                    if (value == null || (value is string s && String.IsNullOrWhiteSpace(s)))
-                    {
-                        continue;
-                    }
-                    if (prop.CustomAttributes.Any())
-                    {
-                        string? dateFormat = HasDateFormatAttribute(prop);
+                        DateFormatAttribute? dateFormat = att as DateFormatAttribute;
                         if (dateFormat != null)
                         {
-                            ws.Cell(row, col).Value = ((DateTime)value).ToString(dateFormat ?? "dd/MM/yyyy");
-                            continue;
-                        }
-
-                        string[]? trueFalse = HasTrueFalseAttribute(prop);
-                        if (trueFalse != null)
-                        {
-                            ws.Cell(row, col).Value = ((bool)value) == true ? trueFalse[0] : trueFalse[1];
-                            continue;
+                            return dateFormat.Format;
                         }
                     }
-                    ws.Cell(row, col).Value = value.ToString();
                 }
+                return null;
+            }
+            private string[]? HasTrueFalseAttribute(PropertyInfo prop)
+            {
+                var attributes = prop.GetCustomAttributes(true);
+                if (attributes.Any())
+                {
+                    foreach (var att in attributes)
+                    {
+                        TrueFalseAttribute? truefalse = att as TrueFalseAttribute;
+                        if (truefalse != null)
+                        {
+                            return truefalse.TF.Split('_');
+
+                        }
+                    }
+                }
+                return null;
             }
 
-            ws.Columns().AdjustToContents();
-            var M = new MemoryStream();
-            wbook.SaveAs(M);
-            return M.ToArray();
+            private PropertyInfo[] GetPropertiesOrderedByAttribute(PropertyInfo[] props)
+            {
+                bool AreOrdered = props.Any(x => x.CustomAttributes.Any(z => z.AttributeType == typeof(ColumnOrderAttribute)));
+                if (AreOrdered)
+                {
+                    props = props
+                          .Select(x => new { prop = x, ord = x.GetCustomAttribute<ColumnOrderAttribute>()?.Order })
+                          .OrderBy(z => z.ord == null).ThenBy(z => z.ord).Select(a => a.prop).ToArray();
+                }
+                return props;
+            }
+            public byte[] Create<T>(List<T> items)
+            {
+                var wbook = new XLWorkbook();
+
+                var ws = wbook.Worksheets.Add(SheetName);
+                int row = 1;
+                if (!String.IsNullOrWhiteSpace(this.SheetTitle))
+                {
+                    ws.Cell(row, 1).SetValue(this.SheetTitle);
+                    ws.Cell(row, 1).Style.Font.FontSize = 20;
+                    row++;
+                }
+                if (!String.IsNullOrWhiteSpace(this.SheetSubTitle))
+                {
+                    ws.Cell(row, 1).SetValue(this.SheetSubTitle);
+                    ws.Cell(row, 1).Style.Font.FontSize = 16;
+                    row++;
+                }
+                var props = typeof(T).GetProperties();
+
+                props = GetPropertiesOrderedByAttribute(props);
+
+
+                int col = 1;
+                foreach (var prop in props)
+                {
+                    ws.Cell(row, col).SetValue(HasColumnNameAttribute(prop) ?? prop.Name);
+                    ws.Cell(row, col).Style.Font.Bold = true;
+                    ws.Cell(row, col).Style.Fill.SetBackgroundColor(XLColor.LightGray);
+                    col++;
+                }
+
+
+                foreach (var item in items)
+                {
+                    row++;
+                    col = 0;
+                    foreach (var prop in props)
+                    {
+                        col++;
+                        object? value = item?.GetType().GetProperty(prop.Name)?.GetValue(item);
+                        if (value == null || (value is string s && String.IsNullOrWhiteSpace(s)))
+                        {
+                            continue;
+                        }
+                        if (prop.CustomAttributes.Any())
+                        {
+                            string? dateFormat = HasDateFormatAttribute(prop);
+                            if (dateFormat != null)
+                            {
+                                ws.Cell(row, col).Value = ((DateTime)value).ToString(dateFormat ?? "dd/MM/yyyy");
+                                continue;
+                            }
+
+                            string[]? trueFalse = HasTrueFalseAttribute(prop);
+                            if (trueFalse != null)
+                            {
+                                ws.Cell(row, col).Value = ((bool)value) == true ? trueFalse[0] : trueFalse[1];
+                                continue;
+                            }
+                        }
+                        ws.Cell(row, col).Value = value.ToString();
+                    }
+                }
+
+                ws.Columns().AdjustToContents();
+                var M = new MemoryStream();
+                wbook.SaveAs(M);
+                return M.ToArray();
+            }
         }
     }
+   
 }
